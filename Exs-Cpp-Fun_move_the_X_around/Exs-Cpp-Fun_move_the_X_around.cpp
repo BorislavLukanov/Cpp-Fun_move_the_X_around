fb5cd8948bcec9bc3581f68eaeffe93c1eac6a34
Borislav Lukanov <borislav.lukanov@outlook.com>
2021-08-13 23:28:14 +0300

    up

--- Exs-Cpp-Fun_move_the_X_around/Exs-Cpp-Fun_move_the_X_around.cpp
+++ Exs-Cpp-Fun_move_the_X_around/Exs-Cpp-Fun_move_the_X_around.cpp
@@ -4,23 +4,30 @@
 
 void GetPosition();
 void DispMatrix();
-int UpdatePosition(char input);
-int CheckForCollision();
-
-char matrix[3][3];
+void UpdatePosition(char input);
+void CheckForCollision();
+struct MyStructure
+{
+	int x;
+	int y;
+};
+MyStructure CurrentPosition;
+char player = 'X';
+char matrix[11][11];
 
 int main()
 {
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 11; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < 11; j++)
 		{
 			matrix[i][j] = ' ';
 		}
 
 	}
-	matrix[1][1] = 'X';
+	matrix[8][8] = 'X';
 	matrix[0][0] = 'O';
+	matrix[4][4] = 'Z';
 
 	char ch = ' ';
 	do
@@ -29,10 +36,10 @@ int main()
 		GetPosition();
 		CheckForCollision();
 		UpdatePosition(ch);
-		std::cout << "\nUse W S A D to move the X around and jet the O.\nSpace to end game.\nGet the Z first and become @ ;D";
+		std::cout << "\nUse W S A D to move the X around and get the O.\nSpace to end game.\nGet the Z first and become @ ;D";
 		ch = getchar();
 		if (ch != 'w' || 's' || 'a' || 'd') std::cout << "Use only FPS controls! WSAD >:D";
-		else UpdatePosition();
+		else UpdatePosition(ch);
 
 	} while (ch != ' '); //This is how you play FPSes >;D
 	std::cout << "YOU PUSHED THE SPACE! .............";
@@ -40,51 +47,75 @@ int main()
 
 void GetPosition()
 {
-	int x, y;
-	for (int i = 0; i < 3; i++)
+	if (player == 'X');
 	{
-		for (int j = 0; j < 3; j++)
+		for (int i = 0; i < 11; i++)
 		{
-			if (matrix[i][j] == 'X')
+			for (int j = 0; j < 11; j++)
 			{
-				x = i;
-				y = j;
-			};
+				if (matrix[i][j] == 'X')
+				{
+					CurrentPosition.x = i;
+					CurrentPosition.y = j;
+				};
+			}
+		}
+	};
+	if (player == '@')
+	{
+		for (int i = 0; i < 11; i++)
+		{
+			for (int j = 0; j < 11; j++)
+			{
+				if (matrix[i][j] == '@')
+				{
+					CurrentPosition.x = i;
+					CurrentPosition.y = j;
+				};
+			}
 		}
 	}
-	std::cout << x << " " << y;
+	
 };
 
 void DispMatrix()
 {
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 11; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < 11; j++)
 		{
 			std::cout << " " << matrix[i][j] << " |";
 		}
 		std::cout << "\n";
-		std::cout << "---|---|---\n";
+		for (int o = 0; o < 11; o++)
+		{
+			std::cout << "---|";
+		}
+		std::cout << "\n";
 
 	}
 
 };
 
-int UpdatePosition(char input)
+void UpdatePosition(char input)
 {
+	/*
 	if (input != ' ')
 	{
 
 	}
 	else return input;
+	*/
 };
 
-int CheckForCollision()
+void CheckForCollision()
 {
-	if (matrix[0][0] == 'X' || '@')
+	/*
+	if (matrix[0][0] == CurrentPosition || matrix[ '@') //position
 	{
 		std::cout << "Got it!"; return 0;
+		player = '@';
 	}
 		
-
+*/
 };
